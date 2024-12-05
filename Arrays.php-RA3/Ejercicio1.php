e4db9b17610d185eef528a6629b9f835d94a5191
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-05 09:26:39 +0100

    ejercicio1

---
+++ Arrays.php-RA3/Ejercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- ) Crea un array asociativo con los siguientes datos y claves.
+nombre: Sara, apellido: Martínez, edad: 23, ciudad: Barcelona.
+Muestra los valores del array anterior utilizando foreach. (1 punto) -->
+<?php
+$persona = array("Sara", "Martinez", 23,"Barcelona");
+echo count($persona);
+?> 
+</body>
+</html>
