5a86e8b8d3e9a057f70026e5fab85f00048ccee5
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-12 10:40:22 +0100

    Ejercicio6-creado

---
+++ Arrays.php-RA3/Ejercicio6.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Un profesor quiere registrar las notas de su clase en un array asociativo. Las notas son
+las siguientes:
+Miguel: 5, Luís: 7, Marta: 10, Isabel: 8, Aitor: 4, Pepe: 1
+Guarda los datos en un array asociaƟvo en el orden previo y muéstralos ordenados de
+mayor a menor -->
+    
+
+<h1>Ejercicio 6</h1>
+<?php
+
+$alumnos = []
+
+
+?>
+
+
+</body>
+</html>
