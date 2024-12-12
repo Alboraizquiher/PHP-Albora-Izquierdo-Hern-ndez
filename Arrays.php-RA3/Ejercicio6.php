18d71881d7647e9e6228fb4eeab65fa3ccc10d3f
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-12 11:29:51 +0100

    acabado el ejercicio6

--- Arrays.php-RA3/Ejercicio6.php
+++ Arrays.php-RA3/Ejercicio6.php
@@ -27,14 +27,19 @@ $alumnos = [
     "pepe"=>1
 
 ];
+function my_sort($a, $b) {
+    if ($a == $b) return 0;
+    return ($a > $b) ? -1 : 1;
+  }
+ uasort($alumnos, "my_sort");
 
-for ($i=0; $i < $alumnos; $i++) { 
-    echo  "Notas de los estudiantes: ";
-    
-}
+ foreach($alumnos as $key => $value){
+
+    echo "" . $key . " => " . $value;
+    echo ",";
 
 
-    
+ };
 
 
 ?>
