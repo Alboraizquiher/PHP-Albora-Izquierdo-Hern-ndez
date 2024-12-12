dfc00d5bd458b7314126e9fddf45239994e8d179
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-12 10:40:32 +0100

    Ejercicio7-creado

---
+++ Arrays.php-RA3/Ejercicio7.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Calcula la media de las notas y muéstrala con solo 2 decimales. Además, muestra los
+nombres de los alumnos cuya nota esté por encima de la media. (1 punto)
+ -->
+    
+</body>
+</html>
