8efe27bca6942534d18651ca97ef7e92302f6a91
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-12 10:40:58 +0100

    Ejercicio9-creado

---
+++ Arrays.php-RA3/Ejercicio9.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- Una famosa Ɵenda de cómics ha pedido una aplicación web que permita la gesƟón
+del inventario de su Ɵenda. Modificar tomando como base el fichero
+miTiendaComics.php.
+La Ɵenda parƟrá de 4 cómics iniciales:
+
+Para ello, la Ɵenda pide realizar las siguientes tareas:
+1) Calcular valor almacén total mulƟplicando stock por precio
+a. Implementar función mostrarValorAlmacen(1 punto)
+2) Para la semana del “manga”. Aplica una rebaja del 30% a todos los productos
+que tengan por idioma “japonés”.
+a. Arreglar la función aplicarDescuentoManga() (1 punto)
+
+ -->
+
+</body>
+</html>
