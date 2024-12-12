070fa71d454712b074fc0393e424d136ffe66e78
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-12 10:40:47 +0100

    Ejercicio8-creado

---
+++ Arrays.php-RA3/Ejercicio8.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Busca en el array la nota más alta (debe hacerse mediante código). Muestra la nota y
+el nombre del mejor alumno de la clase. Deberá funcionar para cualquier valor del
+array. (1 punto) -->
+    
+</body>
+</html>
