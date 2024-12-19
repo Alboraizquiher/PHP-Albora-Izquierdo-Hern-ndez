bfd6e92a604ce27419a7cf8f5c35fa6891dd19f0
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2024-12-19 11:28:47 +0100

    ejercicio 7 - modificado

--- Arrays.php-RA3/Ejercicio7.php
+++ Arrays.php-RA3/Ejercicio7.php
@@ -10,7 +10,33 @@
 <!-- Calcula la media de las notas y muéstrala con solo 2 decimales. Además, muestra los
 nombres de los alumnos cuya nota esté por encima de la media. (1 punto)
  -->
-    
+ <h1>Ejercicio 7</h1>
+<?php
+
+$alumnos = [
+
+    "Miguel" => 5,
+    "luis"=> 7,
+    "Marta"=>10,
+    "Isabel"=>8,
+    "Aitor"=>4,
+    "pepe"=>1
+
+];
+
+$media = array_sum($alumnos) /6;
+
+echo  "Media de las notas: " .$media ."\n";
+
+for ($i=0; $i < $alumnos ; $i++) { 
+    if ($alumnos[$i]  > $media ) {
+        echo "Los alumnos con nota por encima de la media son:  ".$alumnos[$i]; 
+    }
+}
+
+
+
+?>
 
 
 </body>
